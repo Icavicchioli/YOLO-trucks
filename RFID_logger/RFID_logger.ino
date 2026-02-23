9c08d0b72e56bbd42f1ab9ad72f3521d733eb342
Ignacio Cavicchioli <104076568+Icavicchioli@users.noreply.github.com>
2026-02-23 10:19:24 -0300

    Update RFID_logger.ino

--- RFID_logger/RFID_logger.ino
+++ RFID_logger/RFID_logger.ino
@@ -1,49 +1,35 @@
 /*
-  RFID Logger MVP - Arduino Nano + 2 x MFRC522
+  RFID Logger MVP - Arduino Nano + 1 x MFRC522
 
   Wiring (Arduino Nano):
-  Shared SPI lines (both readers):
     - SCK  -> D13
     - MISO -> D12
     - MOSI -> D11
+    - SDA/SS -> D10
+    - RST    -> D9
     - GND  -> GND
     - 3.3V -> 3.3V
 
-  Reader A (INGRESS):
-    - SDA/SS -> D10
-    - RST    -> D9
-
-  Reader B (EGRESS):
-    - SDA/SS -> D8
-    - RST    -> D7
-
   Serial output format (115200):
     INGRESS,<UID_HEX>
-    EGRESS,<UID_HEX>
 
   Notes:
   - RC522 is 3.3V only.
   - Use level shifting from Nano 5V outputs to RC522 3.3V inputs.
-
 */
 
 #include <SPI.h>
 #include <MFRC522.h>
 
-constexpr byte SS_INGRESS   = 10;
-constexpr byte RST_INGRESS  = 9;
-constexpr byte SS_EGRESS    = 8;
-constexpr byte RST_EGRESS   = 7;
+constexpr byte SS_PIN  = 10;
+constexpr byte RST_PIN = 9;
 
 constexpr unsigned long DUPLICATE_BLOCK_MS = 1200;
 
-MFRC522 readerIngress(SS_INGRESS, RST_INGRESS);
-MFRC522 readerEgress(SS_EGRESS,  RST_EGRESS);
+MFRC522 reader(SS_PIN, RST_PIN);
 
-String lastIngressUid = "";
-String lastEgressUid = "";
-unsigned long lastIngressTs = 0;
-unsigned long lastEgressTs = 0;
+String lastUid = "";
+unsigned long lastTs = 0;
 
 String uidToHex(const MFRC522::Uid &uid) {
   String out = "";
@@ -55,12 +41,14 @@ String uidToHex(const MFRC522::Uid &uid) {
   return out;
 }
 
-void haltReader(MFRC522 &reader) {
-  reader.PICC_HaltA();
-  reader.PCD_StopCrypto1();
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  reader.PCD_Init();
+  Serial.println("RFID_LOGGER_READY");
 }
 
-void pollReader(MFRC522 &reader, const char *eventName, String &lastUid, unsigned long &lastTs) {
+void loop() {
   if (!reader.PICC_IsNewCardPresent()) return;
   if (!reader.PICC_ReadCardSerial()) return;
 
@@ -68,26 +56,12 @@ void pollReader(MFRC522 &reader, const char *eventName, String &lastUid, unsigne
   unsigned long now = millis();
 
   if (uid != lastUid || (now - lastTs) > DUPLICATE_BLOCK_MS) {
-    Serial.print(eventName);
-    Serial.print(",");
+    Serial.print("INGRESS,");
     Serial.println(uid);
     lastUid = uid;
     lastTs = now;
   }
 
-  haltReader(reader);
-}
-
-void setup() {
-  Serial.begin(115200);
-  SPI.begin();
-  readerIngress.PCD_Init();
-  readerEgress.PCD_Init();
-
-  Serial.println("RFID_LOGGER_READY");
-}
-
-void loop() {
-  pollReader(readerIngress, "INGRESS", lastIngressUid, lastIngressTs);
-  pollReader(readerEgress, "EGRESS", lastEgressUid, lastEgressTs);
+  reader.PICC_HaltA();
+  reader.PCD_StopCrypto1();
 }
